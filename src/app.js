5548d3737348726f02be9150412b480606496cbd
Zachary Shold <sholdzb@gmail.com>
2021-03-30 12:59:35 -0500

    tracks input ingredients in state of app

--- src/app.js
+++ src/app.js
@@ -6,13 +6,21 @@ class App extends React.Component {
     this.state = {
       ingredients: '',
     };
+    this.handleChange = this.handleChange.bind(this);
   }
+  handleChange(e) {
+    e.preventDefault();
+    const id = e.target.id;
+    const text = e.target.value;
+    this.setState({[id]: text});
+  }
+
   render() {
     return (
       <div>
         <div>hungry.io</div>
         <label> what do you have on hand?
-          <input id='ingredients'type='text' placeholder='list ingredients here' />
+          <input id='ingredients'type='text' placeholder='list ingredients here' onChange={this.handleChange} />
         </label>
       </div>
     )
