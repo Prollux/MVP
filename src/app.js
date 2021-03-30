be02a33fcfbf0adfa64b337ddd78822a957eb0d9
Zachary Shold <sholdzb@gmail.com>
2021-03-30 12:55:27 -0500

    restructures html to load scripts after declaring target DOM element

--- src/app.js
+++ src/app.js
@@ -1,18 +1,20 @@
 import React from 'react';
 
-class  App extends React.Component {
+class App extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      ingredients: '',
+    };
   }
-    render() {
-      return (
-        <div>
-          <div>hungry.io</div>
-          <label> what do you have on hand?
-            <input type='text' placeholder='list ingredients here' />
-          </label>
-        </div>
+  render() {
+    return (
+      <div>
+        <div>hungry.io</div>
+        <label> what do you have on hand?
+          <input id='ingredients'type='text' placeholder='list ingredients here' />
+        </label>
+      </div>
     )
   }
 }
